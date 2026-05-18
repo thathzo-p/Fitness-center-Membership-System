4df1849d647323216b46d2f8bf84dd1cd0c72ca4
Kavindu20946 <kavindujayarathna20946@gmail.com>
2026-05-18 12:48:53 +0530

    java fixes

--- src/main/java/com/fitnessmembership/fitnessmembership/model/MemberProgress.java
+++ src/main/java/com/fitnessmembership/fitnessmembership/model/MemberProgress.java
@@ -18,7 +18,7 @@ public class MemberProgress {
         this.progressId = progressId;
         this.memberId = memberId;
         this.date = date;
-        this.weight = weight;
+        this.weight = weight;  //current weight of the user
         this.height = height;
         this.gymDays = gymDays;
         this.bmi = calculateBMI(weight, height);
@@ -30,23 +30,55 @@ public class MemberProgress {
         return Math.round((weight / (heightM * heightM)) * 100.0) / 100.0;
     }
 
-    public String getProgressId() { return progressId; }
-    public String getMemberId() { return memberId; }
-    public String getDate() { return date; }
-    public double getWeight() { return weight; }
-    public double getHeight() { return height; }
-    public int getGymDays() { return gymDays; }
-    public double getBmi() { return bmi; }
-    public String getNotes() { return notes; }
+    public String getProgressId() {
+        return progressId;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+    public String getDate() {
+        return date;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public double getHeight() {//height of the user
+        return height;
+    }
+    public int getGymDays() {
+        return gymDays;
+    }
+    public double getBmi() {
+        return bmi;
+    }
+    public String getNotes() {
+        return notes;
+    }
 
-    public void setProgressId(String progressId) { this.progressId = progressId; }
-    public void setMemberId(String memberId) { this.memberId = memberId; }
-    public void setDate(String date) { this.date = date; }
-    public void setWeight(double weight) { this.weight = weight; }
-    public void setHeight(double height) { this.height = height; }
-    public void setGymDays(int gymDays) { this.gymDays = gymDays; }
-    public void setBmi(double bmi) { this.bmi = bmi; }
-    public void setNotes(String notes) { this.notes = notes; }
+    public void setProgressId(String progressId) {
+        this.progressId = progressId;
+    }
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public void setGymDays(int gymDays) {
+        this.gymDays = gymDays;
+    }
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 
     @Override
     public String toString() {
